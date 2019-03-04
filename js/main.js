d5815b1bfd47a41d430cc59eec8c0ad81745549d
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-03-04 14:29:28 -0500

    Upload files SteakHouse

--- js/main.js
+++ js/main.js
@@ -1,41 +1,10 @@
-//Initialize and add the map
-function initMap() {
-    //Your location
-    const loc = {
-        lat: 45.421532,
-        lng: -75.697189
-    };
-    //Centered map on location
-    const map = new google.maps.Map(document.querySelector('.map'), {
-        zoom: 14,
-        center: loc
-    });
-    //The marker, positioned  at location
-
-    const marker = new google.maps.Marker({
-        position: loc,
-        map: map
-    });
-}
-
 //Sticky Menu background
 window.addEventListener('scroll', function () {
     if (window.scrollY > 150) {
-        document.querySelector('#navbar').style.opacity = 0.9;
-    } else {
-        document.querySelector('#navbar').style.opacity = 1;
-    }
-});
+        document.querySelector('nav').style.backgroundColor = "#262626";
+        document.querySelector('nav').style.backgroundPosition = "fixed";
 
-//Smooth Scrolling
-$('#navbar a, .btn').on('click', function (event) {
-    if (this.hash !== '') {
-        event.preventDefault();
-        const hash = this.hash;
-        $('html, body').animate({
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
+    } else if (window.scrollY < 50) {
+        document.querySelector('nav').style.backgroundColor = "transparent";
     }
 });
