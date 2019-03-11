9216362b6c7e39f6c54e4fae9b196bda3c5b456a
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-03-10 23:26:28 -0400

    more content

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Sticky Menu background
 window.addEventListener('scroll', function () {
     if (window.scrollY > 150) {
-        document.querySelector('nav').style.backgroundColor = "#262626";
-        document.querySelector('nav').style.backgroundPosition = "fixed";
+        document.querySelector('.main-nav').style.backgroundColor = "#262626";
+        document.querySelector('.main-nav').style.backgroundPosition = "fixed";
 
     } else if (window.scrollY < 50) {
         document.querySelector('nav').style.backgroundColor = "transparent";
