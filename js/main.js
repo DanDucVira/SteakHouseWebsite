8f63953be3d1f822054906a5be0b8e7134c46fbe
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-03-14 14:44:44 -0400

    added reponsive menu

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ window.addEventListener('scroll', function () {
     if (window.scrollY > 150) {
         document.querySelector('.main-nav').style.backgroundColor = "#262626";
         document.querySelector('.main-nav').style.backgroundPosition = "fixed";
+        document.querySelector('label').style.backgroundColor = "#262626";
+        document.querySelector('label').style.backgroundPosition = "fixed";
 
     } else if (window.scrollY < 50) {
-        document.querySelector('nav').style.backgroundColor = "transparent";
+        document.querySelector('.main-nav').style.backgroundColor = "transparent";
+        document.querySelector('label').style.backgroundColor = "transparent";
     }
 });
