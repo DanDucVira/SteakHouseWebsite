47d3494ed5a8939f7835e237465c272d0e8118dc
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2019-04-09 12:27:01 -0400

    Reset css and navigation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-//Sticky Menu background
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 150) {
-        document.querySelector('.main-nav').style.backgroundColor = "#262626";
-        document.querySelector('.main-nav').style.backgroundPosition = "fixed";
-        document.querySelector('label').style.backgroundColor = "#262626";
-        document.querySelector('label').style.backgroundPosition = "fixed";
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar-Items')
 
-    } else if (window.scrollY < 50) {
-        document.querySelector('.main-nav').style.backgroundColor = "transparent";
-        document.querySelector('label').style.backgroundColor = "transparent";
-    }
-});
+    navs.forEach(nav => nav.classList.toggle('Navbar-ToggleShow'));
+}
+document.querySelector('.Navbar-Link-toggle')
+    .addEventListener('click', classToggle);
